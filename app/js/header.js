4d1a04a4c205136eb3af583cd39959c7ad3ad77c
Dmitry <dmitrixkovalenko@gmail.com>
2019-10-08 14:02:05 +0300

    Modal navigation window was modified.

--- app/js/header.js
+++ app/js/header.js
@@ -1,17 +1,20 @@
 function nav_overlay() {
-    var nav=document.getElementById('navigation');
+    var nav = document.getElementById('navigation');
     document.getElementById('menu-button').addEventListener("click", function (e) {
-        if(nav.classList[2]==="navigation_hide"){
+        if (nav.classList[2] === "navigation_hide") {
             nav.classList.toggle('navigation_hide');
             nav.classList.toggle('navigation_enable');
+            document.body.style = 'overflow: hidden';
             return;
         }
-        if(document.getElementById('navigation').classList[2]==="navigation_enable"){
+        if (document.getElementById('navigation').classList[2] === "navigation_enable") {
             nav.classList.toggle('navigation_hide');
             nav.classList.toggle('navigation_enable');
+            document.body.style = 'overflow: visible';
         }
-        else{
+        else {
             nav.classList.toggle('navigation_enable');
+            document.body.style = 'overflow: hidden';
         }
     })
 }
@@ -20,7 +23,7 @@ function submenu_overlay() {
     var submenu = document.querySelectorAll('.navigation__arrow-down');
     submenu.forEach(function (elem) {
             elem.parentNode.addEventListener("click", function (e) {
-                if(document.documentElement.clientWidth<992){
+                if (document.documentElement.clientWidth < 992) {
                     e.target.parentElement.lastElementChild.classList.toggle('navigation__submenu_enable');
                 }
             });
